d133c8dafab4a06c0a85f1134077e4e366ec8d68
Edmond Thompson <edmond.j.thompson@gmail.com>
2019-03-10 09:18:02 -0400

    add content to recipe components

--- recipebox/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipebox/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Chicken', 'Test chicken recipe', 'https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940 ')
+  ];
 
   constructor() { }
 
