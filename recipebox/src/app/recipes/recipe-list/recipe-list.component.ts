26cbe5b08eafe952def39f789ef4c45f20e8656b
Edmond Thompson <edmond.j.thompson@gmail.com>
2019-03-10 09:30:17 -0400

    output list of recipes
    *ngFor

--- recipebox/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipebox/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
+    new Recipe('Chicken', 'Test chicken recipe', 'https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940 '),
+    new Recipe('Chicken', 'Test chicken recipe', 'https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940 '),
     new Recipe('Chicken', 'Test chicken recipe', 'https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940 ')
   ];
 
